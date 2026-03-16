612fa1f4ffe927d027737e6d039a51a8aafcd99b
Vinoth Kannan <svkn.87@gmail.com>
2026-03-16 15:31:48 +0530

    feat: Display GitHub contribution graph within a new Card component with enhanced UI and refactor its data fetching to a client-side approach.

--- components/github-contribution-graph.tsx
+++ components/github-contribution-graph.tsx
@@ -6,41 +6,43 @@ import {
   ContributionGraphCalendar,
   ContributionGraphFooter,
 } from "@/components/kibo-ui/contribution-graph";
-import { eachDayOfInterval, endOfYear, formatISO, startOfYear } from "date-fns";
 import { cn } from "@/lib/utils";
-import { unstable_cache } from "next/cache";
 import type { Activity } from "@/components/kibo-ui/contribution-graph"
+import { useEffect, useState } from "react";
 
 type GitHubContributionsResponse = {
   contributions: Activity[]
 }
 
 const username = 'vinothkannans';
-const getCachedContributions = unstable_cache(
-  async () => {
-    const url = new URL(`/v4/${username}`, 'https://github-contributions-api.jogruber.de');
-    const response = await fetch(url);
-    const data = (await response.json()) as GitHubContributionsResponse;
-    return data.contributions;
-  },
-  ['github-contributions'],
-  { revalidate: 60 * 60 * 24 },
-);
+const getCachedContributions = async () => {
+  const url = new URL(`/v4/${username}?y=last`, 'https://github-contributions-api.jogruber.de');
+  const response = await fetch(url);
+  const data = (await response.json()) as GitHubContributionsResponse;
+  return data.contributions;
+}
 
-export default async function GitHubContributionGraph() {
-  const data = await getCachedContributions();
+export default function GitHubContributionGraph() {
+  const [data, setData] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    getCachedContributions().then((contributions) => {
+      setData(contributions);
+    });
+  }, []);
+
   return (
-    <ContributionGraph data={data}>
+    <ContributionGraph data={data} className="mx-auto">
       <ContributionGraphCalendar>
         {({ activity, dayIndex, weekIndex }) => (
           <ContributionGraphBlock
             activity={activity}
             className={cn(
-              'data-[level="0"]:fill-[#ebedf0] dark:data-[level="0"]:fill-[#161b22]',
-              'data-[level="1"]:fill-[#9be9a8] dark:data-[level="1"]:fill-[#0e4429]',
-              'data-[level="2"]:fill-[#40c463] dark:data-[level="2"]:fill-[#006d32]',
-              'data-[level="3"]:fill-[#30a14e] dark:data-[level="3"]:fill-[#26a641]',
-              'data-[level="4"]:fill-[#216e39] dark:data-[level="4"]:fill-[#39d353]'
+              'data-[level="0"]:fill-muted dark:data-[level="0"]:fill-muted',
+              'data-[level="1"]:fill-chart-2 dark:data-[level="1"]:fill-chart-2',
+              'data-[level="2"]:fill-chart-3 dark:data-[level="2"]:fill-chart-3',
+              'data-[level="3"]:fill-chart-4 dark:data-[level="3"]:fill-chart-4',
+              'data-[level="4"]:fill-chart-5 dark:data-[level="4"]:fill-chart-5'
             )}
             dayIndex={dayIndex}
             weekIndex={weekIndex}
