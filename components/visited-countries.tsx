6a53dfd8ab856bff3dea50626cc754a328bfcb81
Vinoth Kannan <svkn.87@gmail.com>
2026-03-30 14:11:35 +0530

    style: adjust scale and margins of WorldMap component in VisitedCountries

--- components/visited-countries.tsx
+++ components/visited-countries.tsx
@@ -2,7 +2,7 @@ import { WorldMap } from "./shadcnmaps/maps/world";
 
 export default function VisitedCountries() {
   return (
-    <WorldMap regions={[
+    <WorldMap className="scale-150 -ml-20 -mt-20" regions={[
       { id: 'IN', className: 'fill-chart-3 hover:fill-chart-4' },
       { id: 'SG', className: 'fill-chart-5 hover:fill-chart-4' },
       { id: 'TH', className: 'fill-chart-5 hover:fill-chart-4' },
